01c2066fc8de44f6e034f5dc8f5acd96d00fb207
MathildeBettembourg <mathilde.bettembourg@gmail.com>
2022-12-01 10:00:33 +0100

    first commit

---
+++ src/test/java/fr/mebg/formation/spring/competences/mycomp/MycompApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mebg.formation.spring.competences.mycomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
